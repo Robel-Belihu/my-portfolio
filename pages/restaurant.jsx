7cb65152d5d756675ab3a6ab87fde2ce3f94050d
Robel-Belihu <robelmelalku334@gmail.com>
2023-05-11 20:02:55 +0300

    add some more components

--- pages/restaurant.jsx
+++ pages/restaurant.jsx
@@ -28,22 +28,18 @@ const recipes = () => {
           <p>Project</p>
           <h2>Overview</h2>
           <p>
-            Built with React JS and Tailwindcss. Global recipes is a recipe hub
-            which utilizes Forkify API to bring users the details of their
-            favorite meals. Features include <br />
-            - Responsive UI Unlimited searching for foods which brings back
-            their description and ingredients. <br /> - Favorites page where the
-            users store there favorite recipes.
+            Simple and fully responsive Restaurant landing page with booking and
+            menu options.
           </p>
           <a
-            href="https://github.com/Robel-Belihu/global-recipes.git"
+            href="https://github.com/Robel-Belihu/Restaurant-Landing-page.github.io.git"
             target="_blank"
             rel="noreferrer"
           >
             <button className="px-8 py-2 mt-4 mr-8">Code</button>
           </a>
           <a
-            href="https://global-recipes.vercel.app/"
+            href="https://restaurant-landin-page.netlify.app/"
             target="_blank"
             rel="noreferrer"
           >
@@ -55,13 +51,13 @@ const recipes = () => {
             <p className="text-center font-bold pb-2">Technologies</p>
             <div className="grid grid-cols-3 md:grid-cols-1">
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> React
+                <RiRadioButtonFill className="pr-1" /> HTML
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Javascript
+                <RiRadioButtonFill className="pr-1" /> CSS/Bootstrap
               </p>
               <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill className="pr-1" /> Firebase/Firestore
+                <RiRadioButtonFill className="pr-1" /> JavaScript
               </p>
             </div>
           </div>
